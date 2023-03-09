4d81d74959f0f43778f3ac6e8a37f4b8abfb2fed
kamblenilesh <nileshkamble029@gmail.com>
2023-03-09 16:00:50 +0530

    Routers and Filters

--- app.js
+++ app.js
@@ -10,25 +10,22 @@ const bodyParser = require('body-parser');
 // Creating express application,   express as function
 const app = express();
 
+// importing admin-router object
+const adminRoutes = require('./routes/admin');
+
+// importing shop-router object
+const shopRoutes = require('./routes/shop');
+
 // send through a form
 app.use(bodyParser.urlencoded({extended: false}));
 
-// object, add use method which handle requestHandler, next is function
-app.use('/add-product',(req, res, next) => { // next() is not use
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>');
-});
+// call obj file
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-// Execution from top to bottom
-// filtering request like post, get, del
-app.post('/product', (req, res, next) => {
-    console.log(req.body); // Extracting user send
-    res.redirect('/');  // redirect to '/'
-});
-
-// get, post, del get extract match not 'use' method
-app.use('/',(req, res, next) => {
-    // console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>'); // Allows to send a response & no need to next() fun.
+// Error Page
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
 });
 
 app.listen(4000);
--- routes/admin.js
+++ routes/admin.js
@@ -4,14 +4,17 @@ const express = require('express');
 // import router using module
 const router = express.Router();
 
-// object, add use method which handle requestHandler, next is function
+// object, add get method which handle requestHandler, next is function
+// GET request
 router.get('/add-product',(req, res, next) => { // next() is not use
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>');
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>');
 });
 
 // Execution from top to bottom
 // filtering request like post, get, del
-router.post('/product', (req, res, next) => {
+// two different path due to method (in 1. get & 2. post)
+// POST request
+router.post('/add-product', (req, res, next) => {
     console.log(req.body); // Extracting user send
     res.redirect('/');  // redirect to '/'
 });
